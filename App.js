9c8f79cf0152eed73e59f1e41052e3170e42863c
Sankalp <your.email@example.com>
2025-08-11 16:52:41 +0530

    made app

--- App.js
+++ App.js
@@ -1,31 +1,94 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-const k = React.createElement("h1",
-    {id:"h1"},
-    "hola"
-)
-const number = 1122;
+/* Components of Our Food-Order App
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search Bar
+ * - Restaurant-Container
+ *  - Restaurant-Card
+ *    - Img
+ *    - Name of Res, Star Rating, cuisine, delivery time.
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ */
 
-const Heading1 = function () {(           <h1>yo man
-     </h1>   )}
+const Header = () => (
+  <div className="header">
+    <div className="logo">
+      <img
+        className="img"
+        src="https://t3.ftcdn.net/jpg/08/29/90/88/360_F_829908823_kYsRKdQcIaYEAhHRAZTIXuSKvuVPif8w.jpg"
+        alt="logo"
+      />
+    </div>
+    <div className="navitems">
+      <ul className="ul">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  </div>
+);
 
-const Heading = ()=>  
-<>
+const RestroCard = (propObj) => {
+  console.log(propObj.rating);
+  return (
+    <div className="restro-card">
+      <div className="restro-card-content">
+        <div className="restro-card-img-div">
+          <img
+            className="restro-card-img"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpK1noS9RwpA351YDfG9dRCvSON-j5nZHU0A&s"
+            alt=""
+          />
+        </div>
+        <div className="restro-info">
+          <div className="restro-name">
+            <h3>Anitas Kitchen</h3>
+          </div>
+          <div className="restro-dishesh">
+            {" "}
+            <h4>Poolav ,Khichdi ,Idli ,Dosa</h4>
+          </div>
+          <div className="restro-time-rating">
+            <div className="testro-time">39 Mins</div>
+            <div className="restro-rating">
+              {propObj.rating === undefined ? "4.5" : propObj.rating}
+            </div>
+          </div>
+          <div className="restro-knowmore">
+            <button className="restro-knowmore-btn">know more</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+const Body = () => (
+  <div className="body">
+    <div className="serch-bar"></div>
+    <div className="restro-container">
+      <RestroCard />
+      <RestroCard restroname="sankalp" rating="3.4" />
+    </div>
+  </div>
+);
 
-<Heading1/>
-<h1>
-    yo girl
+const App = () => (
+  <div className="App">
+    <Header />
+    <Body />
+  </div>
+);
 
-</h1>
- <p>wassup {number * 9999999999}</p></>;
-const jsx = <h1>this is jsx in single linke without function</h1>
-
-const Tit = function (){return( <> <Heading/> <h1>  yoda</h1> </>    )
-}
-
-
-
-const j = document.getElementById("root")
-const root = ReactDOM.createRoot(j);
-root.render(<Tit/>)
+const j = 2;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
